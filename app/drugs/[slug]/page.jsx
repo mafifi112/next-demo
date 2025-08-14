57afd7ee7010cde9c6fef3c92e64022e8352096b
mfifi112 <mafifi11294@gmail.com>
2025-08-14 21:25:32 +0300

    f commit

--- app/drugs/[slug]/page.jsx
+++ app/drugs/[slug]/page.jsx
@@ -5,26 +5,27 @@ export async function generateStaticParams() {
 
   for (const drug of drugs) {
     if (drugClasses.includes(drug.classification.en)) continue;
-    drugClasses.push(drug.classification.en)
+    drugClasses.push(drug.classification.en);
   }
-  
+
   return drugClasses.map((key) => {
-    key = encodeURI(key)
-    return { slug: key};
+    // key = encodeURI(key);
+    return { slug: key };
   });
 }
 
 export default async function DrugsPage({ params }) {
   const { slug } = await params;
-    console.log(decodeURIComponent(slug));
-    
+
   return (
     <div className="grid">
-        {drugs.filter(drug => drug.classification.en === decodeURIComponent(slug)).map(drug => (
-            <div key={drug.drug_name.en}>
-                <h2>{drug.drug_name.en}</h2>
-            </div>
+      {drugs
+        .filter((drug) => drug.classification.en === decodeURIComponent(slug))
+        .map((drug) => (
+          <div key={drug.drug_name.en}>
+            <h2>{drug.drug_name.en}</h2>
+          </div>
         ))}
     </div>
-  )
+  );
 }
